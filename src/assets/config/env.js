a1fbd9bfd5c051e5d06acce8aba52015b848e730
gourish naik <gourish@campionsoftware.com>
2022-07-08 11:57:05 +0530

    commited

--- src/assets/config/env.js
+++
@@ -1,8 +0,0 @@
-/// it will be replaced, do not remove this , this is need when you work from locally 
-
-(function(window) {
-    window.env = window.env || {};
-  
-    console.log('Loadded env ', window["env"])
-
-  })(this);
